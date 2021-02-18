7497b261cae5b6ecc61a12f09e5099257905469c
patryckLisboa <patryck-lisboa@hotmail.com>
2021-02-18 00:18:47 -0200

    Commit inicial

---
+++ src/test/java/com/patryck/snaplife/SnaplifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryck.snaplife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnaplifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
